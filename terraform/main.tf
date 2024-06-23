fa0448f77992a8fe995df5ec92b0750e9de09782
mnik7044 <mnikwork7802@gmail.com>
2024-06-24 03:09:19 +0530

    elastic ip added changed region

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "ap-south-1a"  # Adjust as needed
+  availability_zone       = "ap-southeast-1a"  # Adjust as needed
   map_public_ip_on_launch = true
 
   tags = {
